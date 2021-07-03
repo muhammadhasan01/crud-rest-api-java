8d5b5d8bf892079140efd01c09cce32a019c2e8e
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-03 14:21:47 +0700

    feat: add implementation for PersonService

---
+++ src/main/java/com/example/crudrestapi/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.example.crudrestapi.service;
+
+import com.example.crudrestapi.dao.PersonDao;
+import com.example.crudrestapi.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+}
