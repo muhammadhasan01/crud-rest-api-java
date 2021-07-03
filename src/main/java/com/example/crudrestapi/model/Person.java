a2015c9610883163568b647e780dc017dd72de2f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-03 13:49:38 +0700

    feat: add implementation for person model

--- src/main/java/com/example/crudrestapi/model/Person.java
+++ src/main/java/com/example/crudrestapi/model/Person.java
@@ -1,5 +1,11 @@
 package com.example.crudrestapi.model;
 
-public class Person {
+import lombok.Data;
 
+import java.util.UUID;
+
+@Data
+public class Person {
+    private final UUID id;
+    private final String name;
 }
