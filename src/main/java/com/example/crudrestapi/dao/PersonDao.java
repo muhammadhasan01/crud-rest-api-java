7d8fe809c8e149be801ac0e0a422138b2cc8be16
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-03 13:49:46 +0700

    feat: add implementation for person DAO

---
+++ src/main/java/com/example/crudrestapi/dao/PersonDao.java
@@ -0,0 +1,19 @@
+package com.example.crudrestapi.dao;
+
+import com.example.crudrestapi.model.Person;
+
+import java.util.UUID;
+
+/**
+ * The actual interface/contract for everyone
+ * that wishes to implement this interface
+ */
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
