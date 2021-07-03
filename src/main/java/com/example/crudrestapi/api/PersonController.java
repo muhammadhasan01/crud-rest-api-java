4c9b212a85115488e7490b9be4dc7b9ff2294282
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-03 16:25:14 +0700

    feat: add validators for person

--- src/main/java/com/example/crudrestapi/api/PersonController.java
+++ src/main/java/com/example/crudrestapi/api/PersonController.java
@@ -3,8 +3,10 @@ package com.example.crudrestapi.api;
 import com.example.crudrestapi.model.Person;
 import com.example.crudrestapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,7 +22,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person) {
+    public void addPerson(@Valid @NonNull @RequestBody Person person) {
         personService.addPerson(person);
     }
 
@@ -40,7 +42,7 @@ public class PersonController {
     }
 
     @PutMapping(path = "{id}")
-    public void deletePersonById(@PathVariable("id") UUID id, @RequestBody Person newPerson) {
-        personService.updatePerson(id, newPerson);
+    public void deletePersonById(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person person) {
+        personService.updatePerson(id, person);
     }
 }
--- src/main/java/com/example/crudrestapi/model/Person.java
+++ src/main/java/com/example/crudrestapi/model/Person.java
@@ -2,10 +2,12 @@ package com.example.crudrestapi.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Data
 public class Person {
     private final UUID id;
+    @NotBlank
     private final String name;
 }
