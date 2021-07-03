12f7e6486a7c8d135c7434d1e2338be22b2f247d
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-03 13:42:42 +0700

    build: add initial boilerplate

--- src/main/java/com/example/crudrestapi/CrudRestApiApplication.java
+++ src/main/java/com/example/crudrestapi/CrudRestApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRestApiApplication.class, args);
+    }
 
 }
